bd54948f3d669fcf1c55a65663a9bd1ce2e09675
Justin Daniel <justinddaniel@gmail.com>
2017-09-08 19:17:12 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?
+def valid_move?(board, index)
+  if (!position_taken?(board, index) && index.between?(0, 8))
+    true
+  end
 end
 
 def position_taken?(board, index)
